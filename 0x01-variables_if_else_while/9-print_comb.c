48b8c66115a426ab3ec4103c57088ef2a998466a
markbrlaise <markkiguli10@gmail.com>
2022-09-09 07:38:32 -0700

    Hola

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + ', ');
+		++n;
+	}
+	putchar('\n');
+	return (0);
+}
