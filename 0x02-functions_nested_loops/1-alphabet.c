8566ca89579af747d0a116723e52d29033b233bc
markbrlaise <markkiguli10@gmail.com>
2022-09-13 05:30:26 -0700

    Hola

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* more headers goes there */
+/**
+ *main - Entry point
+ *
+ *Description: print '_putchar' and a new line
+ *
+ *Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	ch = 'a';
+	while (ch <= 'z')
+		_putchar(ch);
+}
