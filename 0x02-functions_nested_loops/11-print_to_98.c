6ca8c42448932de87418522af9ca41937b6644eb
markbrlaise <markkiguli10@gmail.com>
2022-09-13 15:15:03 -0700

    Hoola

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * print_to_98 - print n to 98 counts
@@ -11,25 +11,13 @@
 
 void print_to_98(int n)
 {
+	int count;
+
 	if (n > 98)
-	{
-		while (n >= 98)
-		{
-			_putchar(n + 48);
-			_putchar(',');
-			_putchar(' ');
-			--n;
-		}
-	}
-	else 
-	{
-		while (n <= 98)
-		{
-			_putchar(n + 48);
-			_putchar(',');
-			_putchar(' ');
-			++n;
-		}
-	}
-	_putchar('\n');
+		for (count = n; count > 98; --count)
+			printf("%d, ", count);
+	else
+		for (count = n; count < 98; ++count)
+			printf("%d, ", count);
+	printf("98\n");
 }
