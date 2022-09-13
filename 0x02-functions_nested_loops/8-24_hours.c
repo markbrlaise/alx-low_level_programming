add3f962f7ac2f579caa8383f105c19a05113034
markbrlaise <markkiguli10@gmail.com>
2022-09-13 10:28:21 -0700

    Hola

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,19 +9,19 @@ void jack_bauer(void)
 {
 	int minutes = 0;
 	int seconds;
-	
+
 	while (minutes < 24)
 	{
 		seconds = 0;
 		for (seconds = 0; seconds < 60; ++seconds)
 		{
-			_putchar((minutes / 10)+'0');
-			_putchar((minutes % 10)+'0');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
 			_putchar(':');
-			_putchar((seconds / 10)+'0');
-			_putchar((seconds % 10)+'0');
+			_putchar((seconds / 10) + '0');
+			_putchar((seconds % 10) + '0');
 			_putchar('\n');
-		}	
+		}
 		++minutes;
 	}
 }
