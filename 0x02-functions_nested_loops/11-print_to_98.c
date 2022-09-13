2dd15896bf64af7eafe706caf34b9928311f91f7
markbrlaise <markkiguli10@gmail.com>
2022-09-13 15:09:05 -0700

    Hola

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+include "main.h"
+
+/**
+ * print_to_98 - print n to 98 counts
+ *           separated by comma, followed
+ *           by space and number should be
+ *           printed in order
+ *
+ * @n: input
+*/
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			_putchar(n + 48);
+			_putchar(',');
+			_putchar(' ');
+			--n;
+		}
+	}
+	else 
+	{
+		while (n <= 98)
+		{
+			_putchar(n + 48);
+			_putchar(',');
+			_putchar(' ');
+			++n;
+		}
+	}
+	_putchar('\n');
+}
