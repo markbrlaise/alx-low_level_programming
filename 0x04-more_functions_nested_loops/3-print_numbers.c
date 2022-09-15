45f578afa57c22b09d77a593dd9ba852b83ce46f
markbrlaise <markkiguli10@gmail.com>
2022-09-15 13:51:44 -0700

    Hola

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 - 9
+ *             only using _putchar twice
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	int num = 48;
+	while (num <= 57)
+	{
+		_putchar(num);
+		++num;
+}
