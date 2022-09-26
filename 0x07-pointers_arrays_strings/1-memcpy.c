c7d2945106fec5316fd5935bf4a0a1f74317735e
markbrlaise <markkiguli10@gmail.com>
2022-09-26 15:28:14 -0700

    Hola

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+/**
+ *_memcpy - function that copies memory area
+ *
+ * @dest: pointer to the destination address of memory
+ *
+ * @src: pointer to source address of memry to be copied
+ *
+ * return: pointer to destination address
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
