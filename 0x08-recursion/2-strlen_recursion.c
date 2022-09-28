81c47c2e48904f08937be2e2b65f399c0b3596fe
markbrlaise <markkiguli10@gmail.com>
2022-09-28 08:13:43 -0700

    Hola

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+/**
+ *_strlen_recursion - function to find length of a string
+ *
+ * @s: pointer to string
+ *
+ *return: len, length of the string
+ */
+
+
+int _strlen_recursion(char *s)
+{
+	int len;
+	if (*s != NULL)
+	{
+		len++;
+		_strlen_recursion(s + 1);
+	}
+
+	return (len);
+}
