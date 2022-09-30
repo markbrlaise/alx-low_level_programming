7eb1909817942e37588090f0df014ec3632f209e
markbrlaise <markkiguli10@gmail.com>
2022-09-30 08:52:48 -0700

    Hola

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <ctype.h>
 #include <string.h>
 
 /**
- *isNum - function to check whether a string is numeric
- *
- * @num: string to be checked
- *
- * Return: 0(success exit)
- *         1(failure exit)
- */
+ * isNum - check if string array is num
+ * @num: string to check
+ * Return: 0 if it's a number
+ *         1 if it's not a number
+*/
 
 int isNum(char num[])
 {
@@ -20,26 +18,23 @@ int isNum(char num[])
 	{
 		if (!isdigit(num[i]))
 			return (1);
-		else
-			return (0);
 	}
+	return (0);
 }
 
 
 /**
- *main - function to print sum of numbers
+ * main - a program that adds positive numbers
  *
- *@argc: counter
- *@argv: arguments
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
  *
- *Return: 0(for a success exit
- *	  1 (for a failure exit)
- *
- */
+ * Return: Always 0 (Success)
+*/
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, sum;
 
 	if (argc == 1)
 	{
@@ -47,10 +42,13 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
+		sum = 0;
 		for (i = 1; i < argc; i++)
 		{
 			if (isNum(argv[i]) == 0)
+			{
 				sum += atoi(argv[i]);
+			}
 			else
 			{
 				printf("Error\n");
@@ -59,6 +57,5 @@ int main(int argc, char *argv[])
 		}
 		printf("%d\n", sum);
 	}
-
 	return (0);
 }
