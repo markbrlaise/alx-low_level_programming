c4a8d03abca87f5fadceb68a7d42709e006e0bba
markbrlaise <markkiguli10@gmail.com>
2022-09-30 16:19:23 -0700

    Hola

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,1 +1,19 @@
-char *_memset(char *s, char b, unsigned int n);
+#include "main.h"
+/**
+ * _memset -   fills memory with a constant byte
+ * @s: pointer block of memory to fill
+ * @b: value to set
+ * @n: bytes of the memory
+ * Return: dest
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+
+	}
+	return (s);
+}
--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,1 +1,11 @@
-int _abs(int n);
+#include "main.h"
+
+/**
+ * _abs - a function that computes the absolute value of an integer
+ * @n: integer input
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	return (n * ((n > 0) - (n < 0)));
+}
