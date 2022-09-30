58c7b20d09dcc4c965afaef640b0aa9c1bf3d062
markbrlaise <markkiguli10@gmail.com>
2022-09-30 06:49:08 -0700

    Hola

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - function to print name of the program
+ *
+ *@argc: argument count
+ *@argv: argument vectors
+ *
+ *Return: (0) success exit
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
