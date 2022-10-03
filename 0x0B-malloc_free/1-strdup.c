ef87612dfac1e546b8966c0deb1c0fc23a1db4ae
markbrlaise <markkiguli10@gmail.com>
2022-10-03 10:45:26 -0700

    Hola

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,17 +13,18 @@ char *_strdup(char *str)
 	int i;
 	char *s;
 	int d;
-	
+
 	if (str == NULL)
 		return (NULL);
 	d = 0;
 	while (str[d])
 		++d;
-	s = malloc(d * sizeof(char));
+	s = malloc((d + 1) * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < d; i++)
 		s[i] = str[i];
+	s[i] = '\0';
 
 	return (s);
 }
