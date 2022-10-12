050ef63f8dcf8b543d5b05b453cb038615bf0edd
markbrlaise <markkiguli10@gmail.com>
2022-10-12 08:26:20 -0700

    Hola

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,4 +4,16 @@
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
+/**
+ * enum bool - typedef function for bool
+ * @TRUE: 1
+ * @FALSE: 0
+*/
+typedef enum bool
+{
+	TRUE = 1,
+	FALSE = 0
+} bool;
+
+
 #endif /* FUNCTION_POINTERS_H */
