ceabb44404fddd590e26762a273fb116cdaa9be1
markbrlaise <markkiguli10@gmail.com>
2022-10-20 06:55:30 -0700

    Hola

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ *list_len - gets the number of nodes
+ *
+ *@h: pointer to the head(first node)
+ *
+ *REturn: number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t node_count = 1;
+	
+	/*if h is null return  0 lenght*/
+	if (h == NULL)
+		return (0);
+	while (h->next != NULL)
+	{
+		/*go to next node*/
+		h = h->next;
+		/*count node*/
+		++node_count;
+	}
+	return (node_count);
+}
