ed8be6eac8505702147a11a2eae0bc6af1968d02
markbrlaise <markkiguli10@gmail.com>
2022-12-12 03:52:14 -0800

    dynamic yh..

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+int _putchar(char c)
+{
+	return (0);
+}
+
+int _islower(int c)
+{
+	return (0);
+}
+
+int _isalpha(int c){
+	return (0);
+}
+
+int _abs(int n){
+	return (0);
+}
+
+int _isupper(int c){
+	return (0);
+}
+
+int _isdigit(int c){
+	return (0);
+}
+
+int _strlen(char *s){
+	return (0);
+}
+
+void _puts(char *s){}
+
+char *_strcpy(char *dest, char *src){
+	exit(98);
+}
+
+int _atoi(char *s){
+	return (0);
+}
+
+char *_strcat(char *dest, char *src){
+	exit(98);
+}
+
+char *_strncat(char *dest, char *src, int n){
+	exit(98);
+}
+
+char *_strncpy(char *dest, char *src, int n){
+	exit(98);
+}
+
+int _strcmp(char *s1, char *s2){
+	return (0);
+}
+
+char *_memset(char *s, char b, unsigned int n){
+	exit(98);
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n){
+	exit(98);
+}
+
+char *_strchr(char *s, char c){
+	exit(98);
+}
+
+unsigned int _strspn(char *s, char *accept)
+{
+	return (0);
+}
+
+char *_strpbrk(char *s, char *accept){
+	exit(98);
+}
+
+char *_strstr(char *haystack, char *needle){
+	exit(98);
+}
